fcd5ae52e170ca1c3aab10ac51ef59bdec105d4c
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2024-08-26 17:24:13 +0800

    delete columns and other stuff

--- Multicards.swiftpm/CreateSetView.swift
+++ Multicards.swiftpm/CreateSetView.swift
@@ -5,66 +5,75 @@ struct CreateSetView: View {
     @Environment(\.colorScheme) var colorScheme
     @Environment(\.dismiss) var dismiss
     @State var showSheet = false
-    
-    @State private var columns: [Column] = [Column(name: "", values: [""])]
-    
+    @State private var columns: [Column] = [Column(name: "", values: [""]),Column(name: "", values: [""])]
     var userData: UserData
+    @State var showAlert = false
+    @State var alertDesc = ""
     
     var body: some View {
         Form {
             Section("Details") {
                 TextField("Title", text: $set.name)
-                if userData.isLoggedIn{
+                if userData.isLoggedIn {
                     Toggle("Make Public", isOn: $set.isPublic)
                 }
             }
             
-            
-                        
             Section("Table") {
-                Button("Import", systemImage: "square.and.arrow.down")
-                {
+                Button("Import", systemImage: "square.and.arrow.down") {
                     showSheet = true
                 }
+                
                 ScrollView(.horizontal) {
-                    HStack{
-                        Grid{
-                            ForEach($columns){$column in
-                                GridRow{
-                                    TextField("Dimension",text: $column.name)
-                                        .bold()
-                                        .padding(5)
-                                        .background(colorScheme == .dark ? .black : Color(uiColor: .systemGray3))
-                                        .cornerRadius(10)
-                                    ForEach($column.values, id: \.self){$value in
-                                        TextField("Value", text: $value)
-                                            .padding(5)
-                                            .background(Color(uiColor: .systemGray6))
-                                            .cornerRadius(10)
-                                    }
-                                } 
-                            }
-                            GridRow{
-                                Button("Add dimension",systemImage: "plus"){
-                                    var numCards = 0
-                                    for i in columns{
-                                        if i.values.count>numCards{
-                                            numCards = i.values.count
+                    HStack {
+                        Grid {
+                            ForEach($columns) { $column in
+                                GridRow {
+                                    TextField("Dimension", text: $column.name)
+                                        .fontWeight(.medium)
+                                    if columns.firstIndex(where: { $0.id == column.id })! > 1 {
+                                        Button {
+                                            if let index = columns.firstIndex(where: { $0.id == column.id }) {
+                                                columns.remove(at: index)
+                                            }
+                                        } label: {
+                                            Image(systemName: "minus.circle.fill")
+                                                .foregroundStyle(.red)
+                                        }
+                                    }else{
+                                        Button{
+                                            
+                                        }label:{
+                                            Image(systemName: "minus.circle")
+                                                .foregroundStyle(.clear)
                                         }
                                     }
-                                    var newColumn = Column(name: "New Dimension", values: [])
-                                    for _ in 0..<numCards{
-                                        newColumn.values.append("")
+                                    
+                                    Rectangle()
+                                        .fill(Color(uiColor: .systemGray3))
+                                        .frame(width: 3)
+                                    
+                                    ForEach($column.values.indices, id: \.self) { index in
+                                        TextField("Value", text: $column.values[index])
+                                        HStack { Divider() }
                                     }
+                                }
+                                Divider()
+                            }
+                            
+                            GridRow {
+                                Button("Add dimension", systemImage: "plus") {
+                                    let numCards = columns.map { $0.values.count }.max() ?? 0
+                                    let newColumn = Column(name: "New Dimension", values: Array(repeating: "", count: numCards))
                                     columns.append(newColumn)
                                 }
                             }
                         }
-                        Button("Add card",systemImage: "plus"){
-                            for i in columns.indices{
+                        
+                        Button("Add card", systemImage: "plus") {
+                            for i in columns.indices {
                                 columns[i].values.append("")
                             }
-                            
                         }
                     }
                 }
@@ -72,7 +81,14 @@ struct CreateSetView: View {
             
             Section {
                 Button("Create") {
-                    
+                    let names = columns.map { $0.name }
+                    if set.name.isEmpty {
+                        showAlert = true
+                        alertDesc = "Title cannot be blank"
+                    } else if names.contains("") {
+                        showAlert = true
+                        alertDesc = "Dimension name cannot be blank"
+                    }
                 }
                 Button("Cancel", role: .destructive) {
                     dismiss()
@@ -83,10 +99,8 @@ struct CreateSetView: View {
         .sheet(isPresented: $showSheet) {
             ImportView(result: $columns)
         }
+        .alert(isPresented: $showAlert) {
+            Alert(title: Text("Error"), message: Text(alertDesc), dismissButton: .default(Text("OK")))
+        }
     }
-    
-}
-#Preview{
-    CreateSetView(userData: UserData())
-        .preferredColorScheme(.light)
 }
